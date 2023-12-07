23149e609f0f4de053de07ff5d1d413b0117e68e
NahuelNGomez <nngomez@fi.uba.ar>
2023-12-07 16:00:08 -0300

    se añade CRUD

--- app_integrador/urls.py
+++ app_integrador/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('libros/', views.LibroListView.as_view(), name='libros'),
-    # path('libros/create/', views.LibroCreateView.as_view(), name='libro_create'),
-    # path('libros/<int:pk>/', views.LibroDetailView.as_view(), name='libro_detail'),
-    # path('libros/<int:pk>/delete/', views.LibroDeleteView.as_view(), name='libro_delete'),
-    # path('libros/<int:pk>/update/', views.LibroUpdateView.as_view(), name='libro_update'),
+    path('listado', views.LibroListView.as_view(), name='libros_listado'),
+    path('create', views.LibroCreateView.as_view(), name='libro_crear'),
+    path('detalle/<int:pk>', views.LibroDetailView.as_view(), name='libro_detalle'),
+    path('modificar/<int:pk>', views.LibroUpdateView.as_view(), name='libro_update'),
+    path('borrar/<int:pk>', views.LibroDeleteView.as_view(), name='libro_borrar'),
 ]
--- app_integrador/views.py
+++ app_integrador/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, CreateView, DetailView, DeleteView, UpdateView
+from .models import Libro
+from django.urls import reverse, reverse_lazy
 
 def index(request):
     return render(request, 'app_integrador/index.html', {})
 
 
-# class LibroListView(ListView):
-#     pass
+class LibroListView(ListView):
+    model = Libro
+    context_object_name = 'listado_libros'
+    template_name = 'app_integrador/libros_listado.html'
 
-# class LibroCreateView(CreateView):
-#     pass
+class LibroCreateView(CreateView):
+    model = Libro
+    template_name = 'app_integrador/libro_crear.html'
+    success_url = 'listado'
+    fields = '__all__'
 
-# class LibroDetailView(DetailView):
-#     pass
 
-# class LibroDeleteView(DeleteView):
-#     pass
+class LibroDetailView(DetailView):
+    model = Libro
+    template_name = 'app_integrador/libro_detalle.html'
+    context_object_name = 'libro'
 
-# class LibroUpdateView():
-#     pass
+class LibroDeleteView(DeleteView):
+    model = Libro
+    template_name = 'app_integrador/libro_borrar.html'
+    success_url = reverse_lazy('libros_listado')
+
+class LibroUpdateView(UpdateView):
+    model = Libro
+    template_name = 'app_integrador/libro_update.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('libro_detalle', kwargs={'pk': self.object.pk})
