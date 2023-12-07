cf444882c2432e06056933fccc5d44d84760b9d3
NahuelNGomez <nngomez@fi.uba.ar>
2023-12-07 12:18:36 -0300

    se añade el modelo de libro

--- TPIntegrador/settings.py
+++ TPIntegrador/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'TPIntegrador.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "tpintegrador",
+        "USER": "postgres",
+        "PASSWORD": "1345",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
--- app_integrador/models.py
+++ app_integrador/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=50, verbose_name='Título')
+    autor = models.CharField(max_length=50, verbose_name='Autor')
+    editorial = models.CharField(max_length=50, verbose_name='Editorial')
+    genero = models.CharField(max_length=50, verbose_name='Género')
+    precio = models.IntegerField(verbose_name='Precio')
+    #stock = models.IntegerField()
+    #imagen = models.ImageField(upload_to='libros', null=True, blank=True)
